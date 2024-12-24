4d2c5f3059fc99e51190ba70d852e5e98e95b037
zyc-123 <58675238+zyc-123@users.noreply.github.com>
2024-12-24 10:32:44 +0800

    Delete models/SDEs directory

--- models/SDEs/__init__.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-def create_sde(nets, sde_opt):
-    sde_class_name = sde_opt['class_name']
-    modules = importlib.import_module(f'{__package__}.{sde_class_name}')
-    create_fn = getattr(modules, 'create_' + sde_class_name)
-
-    return create_fn(nets, sde_opt)
